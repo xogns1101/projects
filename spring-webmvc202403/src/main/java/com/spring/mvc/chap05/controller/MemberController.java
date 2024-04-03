6a1b60a7b48b1190aa2cdcca958c2e826e42c220
dudtjq <dudtjq0415@naver.com>
2024-04-03 16:40:43 +0900

    회원 관리 로그인 처리

--- spring-webmvc202403/src/main/java/com/spring/mvc/chap05/controller/MemberController.java
+++ spring-webmvc202403/src/main/java/com/spring/mvc/chap05/controller/MemberController.java
@@ -1,12 +1,16 @@
 package com.spring.mvc.chap05.controller;
 
+import com.spring.mvc.chap05.dto.request.LoginRequestDTO;
 import com.spring.mvc.chap05.dto.request.SignUpRequestDTO;
+import com.spring.mvc.chap05.service.LoginResult;
 import com.spring.mvc.chap05.service.MemberService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/members")
@@ -52,6 +56,43 @@ public class MemberController {
         return "redirect:/board/list";
 
     }
+
+    // 로그인 양식 화면 요청
+    @GetMapping("/sign-in")
+    public void signInView(){
+
+        System.out.println("/members/sign-in : GET");
+
+    }
+
+    // 로그인 검증 요청
+    @PostMapping("/sign-in")
+    public String signIn(LoginRequestDTO dto
+                 // Model 에 담긴 데이터는 리다이렉트 시 jsp 로 전달되지 못한다
+                 // 리다이렉트는 응답이 나갔다가 재요청이 들어오는데, 그 과정에서
+                 // 첫번째 응답이 나가는 순간 모델은 소멸함.
+                 //(Model 의 생명주기는 한 번의 요청과 응답 사이에서만 유효)
+                , RedirectAttributes ra){
+
+        
+        System.out.println("/members/sign-in : POST");
+        System.out.println("dto = " + dto);
+
+        LoginResult result = memberService.authenticate(dto);
+        // 확인 용도
+        System.out.println("result = " + result);
+
+        ra.addFlashAttribute("result", result);
+        
+        if(result == LoginResult.SUCCESS){ // 로그인 성공
+            return "redirect:/board/list";
+        }
+        
+        return  "redirect:/members/sign-in"; // 로그인 실패시
+
+
+    }
+
 
 
 
