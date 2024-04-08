2b42c512dbd5dce41fc471a8f9945f58f10fb49e
dudtjq <dudtjq0415@naver.com>
2024-04-08 15:52:19 +0900

    공공데이터 API 사용 방법(기상청 단기예보)

--- spring-webmvc202403/src/main/java/com/spring/mvc/chap05/service/WeatherService.java
+++ spring-webmvc202403/src/main/java/com/spring/mvc/chap05/service/WeatherService.java
@@ -70,41 +70,39 @@ public class WeatherService {
             conn.disconnect();
            // System.out.println(sb.toString());
 
-            // StringBuilder 를 String 으로 변환
-            String jsonString  = sb.toString();
+            // StringBuilder를 String으로 변환
+            String jsonString = sb.toString();
 
             JSONParser parser = new JSONParser();
 
-            // String 객체를 JSON 객체로 변경해 줌
+            // String 객체를 JSON객체로 변경해 줌.
             JSONObject jsonObject = (JSONObject) parser.parse(jsonString);
 
-            // "response" 라는 이름의 키에 해당하는 JSON 데이터를 가져 오기
+            // "response"라는 이름의 키에 해당하는 JSON 데이터를 가져옵니다.
             JSONObject response = (JSONObject) jsonObject.get("response");
 
-            // response 안에서 body 키에 해당하는 JSON 데이터를 가져오기
+            // response 안에서 body 키에 해당하는 JSON 데이터를 가져옵니다.
             JSONObject body = (JSONObject) response.get("body");
 
-            // body 에서 items 를 꺼내기
+            // body에서 items를 꺼내세요.
             JSONObject items = (JSONObject) body.get("items");
-            
-            // item 이라는 키를 가진 JSON 데이터를 가져올건데
-            // item 데이터는 여러 값이기 때문에 배열의 문법을 제공하는 객체로 받는다
+
+            // item이라는 키를 가진 JSON 데이터를 가져올건데,
+            // item 데이터는 여러 값이기 때문에 배열의 문법을 제공하는 객체로 받습니다.
             JSONArray itemArray = (JSONArray) items.get("item");
-            
-            // 반복문을 이용해서 객체를 하나씩 취득한 후에 원하는 로직을 작성하기
+
+            // 반복문을 이용해서 객체를 하나씩 취득한 후에 원하는 로직을 작성합니다.
             for (Object obj : itemArray) {
 
-                // Object 를 JSON 객체로 변환
+                // Object를 JSON객체로 변환
                 JSONObject item = (JSONObject) obj;
-
-                // "category" 키에 해당하는 단일 값을 가져오기
+                // "category" 키에 해당하는 단일 값을 가져옵니다.
                 String category = (String) item.get("category");
-
-                // "fcstValue" 키에 해당하는 단일 값을 가져오기
+                // "fcstValue" 키에 해당하는 단일 값을 가져옵니다.
                 String fcstValue = (String) item.get("fcstValue");
 
-                if(category.equals("TMX") || category.equals("TMN")){
-                    log.info("category : {}, value : {}", category, fcstValue);
+                if(category.equals("TMX") || category.equals("TMN")) {
+                    log.info("category: {}, value: {}", category, fcstValue);
                 }
 
             }
