bdd5f1c602332ae1c334b130ac935d860a683e7d
dudtjq <dudtjq0415@naver.com>
2024-04-10 17:14:28 +0900

    프로젝트 초기 셋팅

--- project/src/main/java/com/camping/project/signup/SignUpController.java
+++ project/src/main/java/com/camping/project/signup/SignUpController.java
@@ -4,6 +4,7 @@ package com.camping.project.signup;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
@@ -11,6 +12,7 @@ public class SignUpController {
 
 
         @GetMapping("/sign-up")
+      //  @ResponseBody
         public String signUp(){
 
             return "sign-up/sign-up";
